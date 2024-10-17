db1db5771d7c593ca6984262a2f168a7f4005cef
RobinAxelsson <63591629+RobinAxelsson@users.noreply.github.com>
2024-10-17 21:13:47 +0200

    recon script nice

--- scripts/recon.sh
+++ scripts/recon.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+if [ "$(uname)" = 'Darwin' ]; then
+    macos=1
+fi
+if [ "$(uname)" = 'Linux' ]; then
+    linux=0
+fi
+
 echo --- User ---
+
 me=$(whoami)
 echo "User: $me"
 
@@ -19,24 +27,37 @@ split_groups(){
 }
 split_groups $(id)
 
-# needs password
-# sudo_output=$(sudo -n -l | tail -n 1)
-# echo "$me may run: $sudo_output"
-
 echo ---- System ----
-if [ "$(uname)" = 'Darwin' ]; then
+
+if [ $mac ]; then
     sw_vers
 fi
-if [ "$(uname)" = 'Linux' ]; then
+if [ $linux ]; then
     cat /etc/os-release
 fi
 
 echo $(uname -v)
 
 echo ---- Ip ----
+
 ifconfig | awk '/^[a-z]/ { iface=$1 } /inet / { print iface, $2 }'
-echo ---- Services ----
+
+echo ---- Servers ----
+
 lsof -i -P -n | grep -e PID -e LISTEN
+
+echo --- DNS ---
+
+if [ $macos ]; then
+    smac scutil --dns
+fi
+if [ $linux ]; then
+    echo "/etc/resolv.conf"
+    cat /etc/resolv.conf
+    echo "/etc/hosts"
+    cat /etc/hosts | grep -v '^#'
+fi
+
 echo ---- Commands ----
 
 check_cmd(){
@@ -70,4 +91,5 @@ commands=(
     "tcpdump"
     "wget"
 )
+
 check_cmd "${commands[@]}"
